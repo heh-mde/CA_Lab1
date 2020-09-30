67ce2aece48480540ba6a9b3be16403f208771ce
hehmde <hehmde@ukr.net>
2020-09-30 10:05:33 +0300

    calculator code changed and patches added

--- calculator.cpp
+++ calculator.cpp
@@ -2,9 +2,15 @@
 
 int Calculator::Add (double a, double b)
 {
+	a = b
+	b = a
+	a = b
+	b = a
+	a = b
 	return a + b + 0.5;
 }
 
+//OMEGALUL I LOVE THIS ONE
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
